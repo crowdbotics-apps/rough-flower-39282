07a3b81171a55397ab2af436f18dbfe5f24c73ba
Crowdbotics <team@crowdbotics.com>
2023-03-08 16:45:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-flower-39282",
+  url: "https://rough-flower-39282.botics.co",
+  api: "https://rough-flower-39282.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
